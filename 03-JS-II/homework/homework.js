8de3662f527a8b00d936897266aeff745144bb5e
jorge <jmaciasvesga@gmail.com>
2020-10-28 08:10:42 -0500

    terminando tarea 03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,14 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+
+  if (x>y){
+    return x;
+  }else if(y>x){
+    return y;
+  }else{
+    return x;
+  }
 }
 
 function saludo(idioma) {
@@ -14,18 +22,40 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma == "aleman"){
+    return "Guten Tag!"
+  }
+  else if(idioma == "mandarin"){
+    return "Ni Hao!"
+  }
+  else if(idioma == "ingles"){
+    return "Hello!"
+  }
+  else{
+    return "Hola!"
+  }
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(numero == 10 || numero == 5){
+      return true
+    }else{
+      return false
+    }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero<50 && numero>20){
+    return true
+  }else{
+    return false
+  }
 }
 
 function esEntero(numero) {
@@ -36,6 +66,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if(numero == Math.round(numero)){
+    return true
+  }else{
+    return false
+  }
 }
 
 function fizzBuzz(numero) {
@@ -43,6 +78,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+
+  if (numero%3 == 0){
+    return "fizz"
+  }
+  else if (numero%5 == 0){
+    return "buzz"
+  }
+  else if(numero%3 == 0 && numero%5 == 0){
+    return "fizz" + "buzz"
+  }else{
+    return numero
+  }
 }
 
 function esPrimo(numero) {
